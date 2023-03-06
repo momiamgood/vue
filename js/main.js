9b7ee3513ee066b096f90bc789ceef64dbd4e8f4
gr411_vokan <forstuding411@gmail.com>
2023-03-06 14:06:37 +0700

    start

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ let app = new Vue({
         disc: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: 1,
+        inventory: 100,
+        onSale: 1
     }
 })
 
