e355cd3b142fb1b3cc3065bd37ad4f501b1dd3de
gr411_vokan <forstuding411@gmail.com>
2023-03-06 13:56:50 +0700

    start

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ let app = new Vue({
     data: {
         product: "Socks",
         disc: "A pair of warm, fuzzy socks",
-        image: "assets/vmSocks-green-onWhite.jpg"
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
 
