de13ef052cd8ae16bc73254f350da32b09da8d37
gr411_vokan <forstuding411@gmail.com>
2023-03-06 13:43:16 +0700

    start

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ let product = "Socks";
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        disc: "A pair of warm, fuzzy socks"
     }
  })
  
