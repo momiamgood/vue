67470a8e2798ca68f302a9811e6437c34f3f933e
gr411_vokan <forstuding411@gmail.com>
2023-03-06 13:49:25 +0700

    start

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let product = "Socks";
 
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        disc: "A pair of warm, fuzzy socks"
+        disc: "A pair of warm, fuzzy socks",
+        image: "assets/vmSocks-green-onWhite.jpg"
     }
- })
- 
+})
+
  
